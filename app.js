b43fef439ed0f330975f411f53c5e9f77b820071
Leonid <leonid.gevorgyan42@gmail.com>
2023-09-21 22:01:34 +0700

    third commit(html/css not working)

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 const bodyParser = require("body-parser");
 const todoRouter = require('./routes/todoRouters')
 
 const router = express.Router();
+
+// view engine setup
+app.use(express.static(__dirname + 'views')); 
 app.set("view engine", "ejs")
 
-app.use(express.static(`/${__dirname}/views`));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public/css/')))
 
 app.use("/", todoRouter);
 
